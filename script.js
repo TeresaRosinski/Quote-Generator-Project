f9b9599ad03f620822816755731fbfab6f855528
TeresaRosinski <TeresaRosinski@users.noreply.github.com>
2020-05-12 17:10:41 -0500

    Type of Source Update

--- script.js
+++ script.js
@@ -16,36 +16,42 @@ let quotes = [
       source: "Albert Einstein", 
       citation: "",
       year: "",
+      typeOfSource: "Scientist"
   },
   {
       quote: "You cannot shake hands with a closed fist.", 
       source: "Indira Ghandi", 
       citation: "",
       year: "",
+      typeOfSource: "Politician / Activist"
   },
   {   
       quote: "What's coming will come, and when it does, we'll face it.",
       source: "Hagrid",
       citation: "Harry Potter and the Goblet of Fire",
       year: "2014",
+      typeOfSource: "Fictional Character"
   },
   {
       quote: "A boss has the title, the leader has the people",
       source: "Simon Sinek",
       citation: "Start with Why",
       year: "2011",
+      typeOfSource: "Author, Strategist"
   },
   {
       quote: "A reader lives a thousand lives before he dies. The man who never reads lives only one.",
       source: "George R.R. Martin",
       citation: "",
       year: "",
+      typeOfSource: "Author"
   },
   {
       quote: "A beautiful day begins with a beautiful mindset",
       source: "Unknown",
       citation: "",
       year: "",
+      typeOfSource: ""
   }
 
   ];
@@ -94,6 +100,10 @@ function printQuote(){
   let htmlYearElement = document.getElementsByClassName("year")[0];
   let yearText = quotes[randomNumber].year; 
   htmlYearElement.innerText = yearText;
+
+  let htmlTypeOfSourceElement = document.getElementsByClassName("typeOfSource")[0];
+  let typeOfSourceText = quotes[randomNumber].typeOfSource; 
+  htmlTypeOfSourceElement.innerText = typeOfSourceText;
 }
 
 /***
